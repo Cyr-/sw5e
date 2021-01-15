f133f7c27f9e88e716e1c1e62dfdf68bea8a66f1
TJ <Cyr-@users.noreply.github.com>
2021-01-15 14:40:12 -0600

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
-const gulp = require('gulp');
-const less = require('gulp-less');
+const gulp = require("gulp");
+const less = require("gulp-less");
 
 /* ----------------------------------------- */
 /*  Compile LESS
@@ -7,24 +7,16 @@ const less = require('gulp-less');
 
 const SW5E_LESS = ["less/**/*.less"];
 function compileLESS() {
-  return gulp.src("less/original/sw5e.less")
-    .pipe(less())
-    .pipe(gulp.dest("./"))
+    return gulp.src("less/original/sw5e.less").pipe(less()).pipe(gulp.dest("./"));
 }
 function compileGlobalLess() {
-  return gulp.src("less/update/sw5e-global.less")
-    .pipe(less())
-    .pipe(gulp.dest("./"))
+    return gulp.src("less/update/sw5e-global.less").pipe(less()).pipe(gulp.dest("./"));
 }
 function compileLightLess() {
-  return gulp.src("less/update/sw5e-light.less")
-    .pipe(less())
-    .pipe(gulp.dest("./"))
+    return gulp.src("less/update/sw5e-light.less").pipe(less()).pipe(gulp.dest("./"));
 }
 function compileDarkLess() {
-  return gulp.src("less/update/sw5e-dark.less")
-    .pipe(less())
-    .pipe(gulp.dest("./"))
+    return gulp.src("less/update/sw5e-dark.less").pipe(less()).pipe(gulp.dest("./"));
 }
 const css = gulp.series(compileLESS, compileGlobalLess, compileLightLess, compileDarkLess);
 
@@ -33,15 +25,12 @@ const css = gulp.series(compileLESS, compileGlobalLess, compileLightLess, compil
 /* ----------------------------------------- */
 
 function watchUpdates() {
-  gulp.watch(SW5E_LESS, css);
+    gulp.watch(SW5E_LESS, css);
 }
 
 /* ----------------------------------------- */
 /*  Export Tasks
 /* ----------------------------------------- */
 
-exports.default = gulp.series(
-  gulp.parallel(css),
-  watchUpdates
-);
-exports.css = css;
+exports.default = css;
+exports.watch = gulp.series(gulp.parallel(css), watchUpdates);
